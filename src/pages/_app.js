022e5d75c7f825a06f8181621dc9efdec632ebf2
Muhammad Tahir Ali <alitahir92786@gmail.com>
2023-08-03 17:02:09 +0500

    selectpost

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
-import '@/styles/globals.css';
-import '@/components/AppBar/AppBar.css';
-import '@/components/CommentPicker/CommentPicker.css';
-import '@/pages/SelectPage/SelectFbPage.css'
-import '@/pages/SelectPost/SelectPost.css';
-import '@/pages/Winner/Winner.css';
-import '@/pages/ChooseWinner/ChooseWinner.css';
-import '@/pages/ChooseOptions/ChooseOptions.css';
-import '@/components/Winners/WinnerOne.css';
-import '@/components/SelectButton/SelectButton.css';
-import '@/components/Header/Header.css';
+import "@/styles/globals.css";
+import "@/components/AppBar/AppBar.css";
+import "@/components/CommentPicker/CommentPicker.css";
+import "@/pages/SelectPage/SelectFbPage.css";
+import "@/pages/SelectPost/SelectPost.css";
+import "@/pages/Winner/Winner.css";
+import "@/pages/ChooseWinner/ChooseWinner.css";
+import "@/pages/ChooseOptions/ChooseOptions.css";
+import "@/components/Winners/WinnerOne.css";
+import "@/components/SelectButton/SelectButton.css";
+import "@/components/Header/Header.css";
+import { StyledEngineProvider } from "@mui/material/styles";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyledEngineProvider injectFirst>
+      <Component {...pageProps} />
+    </StyledEngineProvider>
+  );
 }
