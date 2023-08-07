8e985d564a99d4c82aa817913edae640c3942b19
Muhammad Tahir Ali <alitahir92786@gmail.com>
2023-08-07 17:46:09 +0500

    switch style

--- src/components/ConditionsForm/Switch.jsx
+++ src/components/ConditionsForm/Switch.jsx
@@ -3,17 +3,23 @@ import { styled } from '@mui/material/styles';
 
 const MySwitch = styled(Switch)(({ theme }) => ({
   width: 38,
-  height: 23,
+  height: 20,
   borderRadius: 13,
   padding: 0,
+  boxShadow: "0px 0.5px 9px 3px rgba(0, 0, 0, 0.15)",
   "& .MuiSwitch-thumb": {
     width: "15px",
     height: "15px",
     borderRadius: "50%",
     backgroundColor: "#555555",
-    // boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.23)",
     marginTop: "-6.5px",
     marginLeft: "-4px",
+  },
+  "& .css-5ryogn-MuiButtonBase-root-MuiSwitch-switchBase:hover": {
+    backgroundColor: 'unset'
+  },
+  "& .css-jsexje-MuiSwitch-thumb": {
+    boxShadow: 'none'
   },
   "& .MuiSwitch-switchBase.Mui-checked": {
     transform: "translateX(13px)",
@@ -24,17 +30,12 @@ const MySwitch = styled(Switch)(({ theme }) => ({
     "& + .MuiSwitch-track": {
       opacity: 1,
       backgroundColor: "#F6F9FF",
-      border: "1px #cccecf solid", // Border effect
     },
   },
   "& .MuiSwitch-track": {
-    // opacity: 1,
     backgroundColor: "#F6F9FF",
     borderRadius: 16,
-    height: "87%",
-    border: "2px #cccecf solid",
-    // boxShadow: "0px 3px 3px rgba(0, 0, 0, 0.23)",
-  },
+  }
 }));
 
 export default MySwitch;
