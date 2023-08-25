fdd65ccc63af30814292eb995bf4e3de0422dd75
Muhammad Tahir Ali <alitahir92786@gmail.com>
2023-08-25 11:49:34 +0500

    facebook-login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,22 @@
 
 const LoginPage = () => {
+  const responseFacebook = (response) => {
+    console.log('login-response' , response)
+  }
+
+  const componentClicked = (data) => {
+    console.log('user-data' , data);
+  }
   return (
     <div>
       <h1>Login Page</h1>
+      <FacebookLogin 
+      appId="259412120318115"
+      autoLoad={true}
+      fields="name,email,picture"
+      onClick={componentClicked}
+      callback={responseFacebook}
+      />
     </div>
   );
 };
