3f5816e9f5137ea2ec3acc60514a0f7c964ce621
Muhammad Tahir Ali <alitahir92786@gmail.com>
2023-09-07 16:11:30 +0500

    new changes done

--- src/pages/facebook/winners.jsx
+++ src/pages/facebook/winners.jsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import WinnerOne from "@/components/Winners/WinnerOne";
 import Navbar from "@/components/AppBar/AppBar";
-// import { useRouter } from 'next/router';
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 const Winner = () => {
   const Item = styled(Paper)(({ theme }) => ({
@@ -17,34 +17,62 @@ const Winner = () => {
     color: theme.palette.text.secondary,
   }));
   const [commentsArray, setCommentsArray] = useState(null);
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const serializedData = router.query.data || null;
-  // useEffect(() => {
-  //   if(serializedData) {
-  //     const arrayOfObjects = JSON.parse(decodeURIComponent(serializedData));
-  //     setCommentsArray(arrayOfObjects);
-      // console.log(arrayOfObjects );
-  //   }
-  // }, []);
-  
+  const serializedData = router.query.data || null;
+
+  const handleCopyIframeCode = () => {
+    if(typeof window !== "undefined") {
+      const iframeCode = generateIframeCode(
+        `http://localhost:3000/facebook/winners?data=/${serializedData}`
+      );
+      copyToClipboard(iframeCode);
+      alert("copied to clipboard!");
+    }
+  };
+
+  const generateIframeCode = (url) => {
+    return `<iframe src="${url}" width="600" height="400" frameborder="0" allowfullscreen></iframe>`;
+  };
+
+  const copyToClipboard = (text) => {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+  };
+
+  useEffect(() => {
+    // Check if the code is running on the client-side
+    if (typeof window !== "undefined") {
+      // Code here that relies on the window object
+      // Function to add class to body when inside an iframe
+      if (window.self !== window.top) {
+        document.body.classList.add("inside-iframe");
+      }
+    }
+  }, []);
 
   return (
     <>
       <Navbar />
-      <Container maxWidth sx={{pb: "15px"}}>
+      <Container maxWidth sx={{ pb: "15px" }}>
         <Box className="winner_container">
           <Box className="winner">
             <Typography className="winner_heading">
               And the WINNER is
             </Typography>
             <Typography className="winner_subheading">
-              {commentsArray ? "Congrats! Your winner has been picked!" : "No Winner here"}
+              {commentsArray
+                ? "Congrats! Your winner has been picked!"
+                : "No Winner here"}
               {/* Congrats! Your winner has been picked! */}
             </Typography>
-            <WinnerOne 
-            commentsArray = {commentsArray}
-            setCommentsArray ={setCommentsArray}
+            <WinnerOne
+              commentsArray={commentsArray}
+              setCommentsArray={setCommentsArray}
             />
           </Box>
           <Box className="footer1">
@@ -60,14 +88,21 @@ const Winner = () => {
               <Typography>Share the results</Typography>
               <div className="svg-container"></div>
               <div className="share_link">
-                <Link href="#">https://viralkit.io/c</Link>
+                <Link href="#">https://viralkit.io/</Link>
               </div>
-              <div style={{paddingLeft: "15px", paddingTop: "10px"}}>
+              <div
+                style={{
+                  paddingLeft: "15px",
+                  paddingTop: "10px",
+                  cursor: "pointer",
+                }}
+              >
                 <Image
                   src="/copy.png"
                   width="24"
                   height="24"
                   alt="copy"
+                  onClick={handleCopyIframeCode}
                 ></Image>
               </div>
             </div>
